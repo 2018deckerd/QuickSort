46d2210f5c5dba8df95c0482fa94ed46a0d32a1b
2018deckerd <31222254+2018deckerd@users.noreply.github.com>
2017-11-14 22:02:43 -1000

    Add files via upload

--- QuickSorter.java
+++ QuickSorter.java
@@ -24,46 +24,65 @@ public class QuickSorter {
 		int jIndex = hi;
 		int tempVariable = 0;
 		int pivotElement = arr[lo];
+		boolean complete = true;
 		
 		// loop to check whether the left index has exceeded the right index 
 		while (iIndex <= jIndex) {
 			// moves the left pointer until it is greater than the pivot element
-		while (arr[iIndex] <= pivotElement && iIndex != arr.length - 1) {
+		while (arr[iIndex] < pivotElement) {
 			iIndex++;
-		} 
+			if (iIndex > hi) {
+				tempVariable = arr[lo];
+				arr[lo] = arr[hi];
+				arr[hi] = tempVariable;
+				break;
+			}
+		}
+			if (iIndex > hi) {
+				complete = true;
+				break;
+		}
+	
 		
-		if (iIndex == (arr.length - 1)) {
-			tempVariable = pivotElement;
-			arr[0] = arr[iIndex];
-			arr[iIndex] = tempVariable;
-		}
 		// moves the right pointer until it is less than the pivot value
-		while (arr[jIndex] > pivotElement && jIndex != (lo + 1)) {
+		while (arr[jIndex] >= pivotElement) {
 			jIndex--;
+			if (jIndex == lo) {
+				complete = true;
+				break;
+			}
 		}
+		
 		
 		// Swaps elements to continue with partioning
 		if (iIndex < jIndex) {
 			tempVariable = arr[iIndex];
 			arr[iIndex] = arr[jIndex];
 			arr[jIndex] = tempVariable;
+			iIndex++;
+			jIndex--;
+			complete = false;
 		}
 		
-		if (iIndex == jIndex && jIndex == (arr.length - 1)) {
-			break;
+		if (iIndex == hi && jIndex < iIndex && jIndex != hi) {
+			complete = false;
 		}
+		
 		
 		} 
 		
 		
 		// action to complete if the right index has went over the left index
 		if (jIndex < iIndex) {
-			tempVariable = pivotElement;
-			arr[0] = arr[jIndex];
-			arr[jIndex] = tempVariable;
+		  if (!complete) {
+			tempVariable = arr[lo];
+			arr[lo] = arr[iIndex - 1];
+			arr[iIndex - 1] = tempVariable;
+		  } else {
+			  
+		  }
 		}
-		
-		
+			
 		return jIndex;
 	}
 	
--- QuickSorterTest.java
+++ QuickSorterTest.java
@@ -8,24 +8,36 @@ public class QuickSorterTest {
 
 	@Test
 	public void testQuickSort() {
-		// first test case
-		int[] testArray = {1, 3, 5, 2};
-		QuickSorter.quickSort(testArray, 0, 3);
-		for (int element : testArray) {
-			System.out.print(element + " ");
-		}
-		int[] sorted = {1, 2, 3, 5};
+		// first test case (extreme case where list is reversed)
+		int[] testArray = {7, 5, 2};
+		QuickSorter.quickSort(testArray, 0, 2);
+		int[] sorted = {2, 5, 7};
 		assertTrue(Arrays.equals(testArray, sorted));
-		/**
-		// second test case
+		
+		// second test case (random list of numbers)
 		int[] testArray2 = {7, 3, 4, 9, 5, 10, 1, 6, 12, 2, 11, 8};
 		QuickSorter.quickSort(testArray2, 0, 11);
-		for (int element : testArray2) {
-			System.out.print(element + " ");
-		}
 		int[] sorted2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 		assertTrue(Arrays.equals(testArray2, sorted2));
-		*/
+		
+		// third test case (extreme case where list is already in order)
+		int[] testArray3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+		QuickSorter.quickSort(testArray3, 0, 11);
+		int[] sorted3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+		assertTrue(Arrays.equals(testArray3, sorted3));
+			
+		// fifth test case (another extreme case where list is already in order)
+		int[] testArray5 = {-3, 3, 5};
+		QuickSorter.quickSort(testArray3, 0, 3);
+		int[] sorted5 =  {-3, 3, 5};
+		assertTrue(Arrays.equals(testArray5, sorted5));
+		
+		// sixth test case (random number, quite extreme)
+		int[] testArray6 = {7, 5, -3, 3, -5, 10};
+		QuickSorter.quickSort(testArray6, 0, 5);
+		int[] sorted6 = {-5, -3, 3, 5, 7, 10};
+		assertTrue(Arrays.equals(testArray6, sorted6));
+		
 	}
 
 	@Test
@@ -42,6 +54,26 @@ public class QuickSorterTest {
 		partitionIndex2 = QuickSorter.partition(testArray2, 0, 2);
 		assertTrue(partitionIndex2 == 2);
 		
+	}
+	
+	@Test
+	// tests binary search using the Quick Sort Algorithm
+	public void testBinarySearch() {
+		int[] test1 = {13, 2, 7, 12, 15, 4, 11, 6, 10, 9, 3, 1, 14, 5, 8};
+		QuickSorter.quickSort(test1, 0, 14);
+		assertEquals(0, BinarySearcher.binarySearch(test1, 0, 14, 1));
+		assertEquals(14, BinarySearcher.binarySearch(test1, 0, 14, 15));
+		assertEquals(5, BinarySearcher.binarySearch(test1, 0, 14, 6));
+		int[] test2 = {9, -2, 3, 7};
+		QuickSorter.quickSort(test2, 0, 3);
+		assertEquals(2, BinarySearcher.binarySearch(test2, 0, 3, 7));
+		assertEquals(-1, BinarySearcher.binarySearch(test2, 0, 3, 10));
+		assertEquals(0, BinarySearcher.binarySearch(test2, 0, 3, -2));
+		int[] test3 = {7, 8, 9};
+		QuickSorter.quickSort(test3, 0, 2);
+		assertEquals(0, BinarySearcher.binarySearch(test3, 0, 2, 7));
+		assertEquals(-1, BinarySearcher.binarySearch(test3, 0, 2, 10));
+		assertEquals(2, BinarySearcher.binarySearch(test3, 0, 2, 9));
 	}
 
 }
